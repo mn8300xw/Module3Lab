18035b8f290c8e1ce514a566a7d8bb4b4c79431b
Vanessa M. <mn8300xw@go.minnstate.edu>
2025-09-10 23:58:34 +0000

    initial commit

---
+++ Module3Lab/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace TacoBurritoLab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Taco & Burrito Decision Maker!");
+            Console.Write("How hungry are you (1-10)? ");
+            int hungerLevel = int.Parse(Console.ReadLine());
+
+            // Inside the Main method
+            if (hungerLevel >= 8)
+            {
+                Console.WriteLine("You're super hungry! Order both tacos and burritos.");
+            }
+            else if (hungerLevel >= 5)
+            {
+                Console.WriteLine("You're moderately hungry. Go for tacos!");
+            }
+            else
+            {
+                Console.WriteLine("You're not that hungry. Opt for a burrito.");
+            }
+            
+        // After the if-else block
+            string recommendation = (hungerLevel >= 5) ? "Tacos" : "Burrito";
+                    Console.WriteLine($"I recommend: {recommendation}");
+
+        // After the ternary operator
+            switch (hungerLevel)
+            {
+                case 10:
+                    Console.WriteLine("You're a taco and burrito champion!");
+                    break;
+                case 9:
+                case 8:
+                    Console.WriteLine("Taco time!");
+                    break;
+                case 5:
+                    Console.WriteLine("Burrito it is!");
+                    break;
+                default:
+                    Console.WriteLine("Maybe just grab a snack.");
+                    break;
+            }
+        }
+    }
+}
